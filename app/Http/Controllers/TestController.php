da040180bdc8de19cd2fea6934aed502857589dd
unknown <yousif30303@gmail.com>
2025-06-24 18:15:25 +0400

    error for sonarcloud

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        $unused = "I am never used"; // Unused variable (code smell)
+
+        for ($i = 0; $i < 5; $i++) {
+            if ($i === 3) {
+                if (true) {
+                    if (true) {
+                        if (true) {
+                            return "Too deeply nested"; // Code smell: high cognitive complexity
+                        }
+                    }
+                }
+            }
+        }
+
+        return "Done";
+    }
+}
